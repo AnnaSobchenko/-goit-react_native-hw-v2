c2443b02b6e28ecbbdef73fc3d640d8c19084a94
AnnaSobchenko <annsbchnk@gmail.com>
2023-01-03 00:36:02 +0200

    start create app

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import RegistartionScreen from './Screens/RegistrationScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>goit-react_native-hw-v2</Text> */}
       <StatusBar style="auto" />
+      <RegistartionScreen />
     </View>
   );
 }
